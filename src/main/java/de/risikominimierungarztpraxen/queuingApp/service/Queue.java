b67ef746b0b89b26b8e890b1e16e1713f79ccb21
Philipp Endrulat <philipp.endrulat@thinkproject.com>
2020-03-21 21:41:08 +0100

    add Queue Class

---
+++ src/main/java/de/risikominimierungarztpraxen/queuingApp/service/Queue.java
@@ -0,0 +1,21 @@
+package de.risikominimierungarztpraxen.queuingApp.service;
+
+import java.util.List;
+
+public class Queue {
+
+    private List<Appointment> appointments;
+    private IQueueChange change;
+
+    public void addAppointment(Appointment appointment) {
+
+    }
+
+    public void removeAppointment(Appointment appointment) {
+
+    }
+
+    public void setOnChange(IQueueChange change) {
+
+    }
+}
