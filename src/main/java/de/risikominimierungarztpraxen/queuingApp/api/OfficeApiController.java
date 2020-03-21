e153189c55776699af2faf237f9d06eb2e50ce15
Philipp Endrulat <philipp.endrulat@thinkproject.com>
2020-03-21 17:40:33 +0100

    update java; further service developement; cleanup

--- src/main/java/de/risikominimierungarztpraxen/queuingApp/api/OfficeApiController.java
+++ src/main/java/de/risikominimierungarztpraxen/queuingApp/api/OfficeApiController.java
@@ -3,16 +3,12 @@ package de.risikominimierungarztpraxen.queuingApp.api;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.risikominimierungarztpraxen.queuingApp.model.Office;
 import de.risikominimierungarztpraxen.queuingApp.service.OfficeService;
@@ -22,16 +18,11 @@ import io.swagger.annotations.ApiParam;
 @Controller
 public class OfficeApiController implements OfficeApi {
 
-    private static final Logger log = LoggerFactory.getLogger(OfficeApiController.class);
-
-    private final ObjectMapper objectMapper;
-
     private final HttpServletRequest request;
     private final OfficeService officeService;
 
     @Autowired
-    public OfficeApiController(ObjectMapper objectMapper, HttpServletRequest request, OfficeService officeService) {
-        this.objectMapper = objectMapper;
+    public OfficeApiController(HttpServletRequest request, OfficeService officeService) {
         this.request = request;
         this.officeService = officeService;
     }
