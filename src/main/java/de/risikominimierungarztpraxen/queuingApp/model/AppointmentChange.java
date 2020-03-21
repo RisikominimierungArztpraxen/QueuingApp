ef6d62fb0ebc14fde0d6fca341a7d542c71a7c0e
Philipp Endrulat <philipp.endrulat@thinkproject.com>
2020-03-21 17:46:13 +0100

    change model

--- src/main/java/de/risikominimierungarztpraxen/queuingApp/model/AppointmentChange.java
+++ src/main/java/de/risikominimierungarztpraxen/queuingApp/model/AppointmentChange.java
@@ -6,9 +6,9 @@ import java.util.Objects;
 
 import javax.validation.Valid;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import org.springframework.validation.annotation.Validated;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -19,74 +19,73 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel(description = "change an appointment")
 @Validated
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-03-21T14:46:54.089Z[GMT]")
-public class AppointmentChange   {
-  @JsonProperty("notifications")
-  @Valid
-  private List<String> notifications = null;
+public class AppointmentChange {
+    @JsonProperty("notifications")
+    @Valid
+    private List<AppointmentBaseNotifications> notifications = null;
 
-  public AppointmentChange notifications(List<String> notifications) {
-    this.notifications = notifications;
-    return this;
-  }
-
-  public AppointmentChange addNotificationsItem(String notificationsItem) {
-    if (this.notifications == null) {
-      this.notifications = new ArrayList<String>();
+    public AppointmentChange notifications(List<AppointmentBaseNotifications> notifications) {
+        this.notifications = notifications;
+        return this;
     }
-    this.notifications.add(notificationsItem);
-    return this;
-  }
 
-  /**
-   * Get notifications
-   * @return notifications
-  **/
-  @ApiModelProperty(value = "")
-  
-    public List<String> getNotifications() {
-    return notifications;
-  }
-
-  public void setNotifications(List<String> notifications) {
-    this.notifications = notifications;
-  }
-
-
-  @Override
-  public boolean equals(java.lang.Object o) {
-    if (this == o) {
-      return true;
+    public AppointmentChange addNotificationsItem(AppointmentBaseNotifications notificationsItem) {
+        if (this.notifications == null) {
+            this.notifications = new ArrayList<>();
+        }
+        this.notifications.add(notificationsItem);
+        return this;
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
+
+    /**
+     * Get notifications
+     * @return notifications
+    **/
+    @ApiModelProperty(value = "")
+
+    public List<AppointmentBaseNotifications> getNotifications() {
+        return notifications;
     }
-    AppointmentChange appointmentChange = (AppointmentChange) o;
-    return Objects.equals(this.notifications, appointmentChange.notifications);
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(notifications);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class AppointmentChange {\n");
-    
-    sb.append("    notifications: ").append(toIndentedString(notifications)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(java.lang.Object o) {
-    if (o == null) {
-      return "null";
+    public void setNotifications(List<AppointmentBaseNotifications> notifications) {
+        this.notifications = notifications;
     }
-    return o.toString().replace("\n", "\n    ");
-  }
+
+    @Override
+    public boolean equals(java.lang.Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AppointmentChange appointmentChange = (AppointmentChange) o;
+        return Objects.equals(this.notifications, appointmentChange.notifications);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(notifications);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class AppointmentChange {\n");
+
+        sb.append("    notifications: ").append(toIndentedString(notifications)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(java.lang.Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
 }
