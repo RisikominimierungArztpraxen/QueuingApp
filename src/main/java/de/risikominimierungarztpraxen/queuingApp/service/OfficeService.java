e85cc41fe8081d59671fc502e3d8a1aa879c3a61
fertl <ertl@new-frontiers.de>
2020-03-21 19:40:02 +0100

    Added database and first repository.

--- src/main/java/de/risikominimierungarztpraxen/queuingApp/service/OfficeService.java
+++ src/main/java/de/risikominimierungarztpraxen/queuingApp/service/OfficeService.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import de.risikominimierungarztpraxen.queuingApp.persistence.entities.DoctorsOffice;
+import de.risikominimierungarztpraxen.queuingApp.persistence.repository.DoctorsOfficeRepository;
 import org.springframework.stereotype.Service;
 
 import de.risikominimierungarztpraxen.queuingApp.model.Office;
@@ -11,18 +13,45 @@ import de.risikominimierungarztpraxen.queuingApp.model.Office;
 @Service
 public class OfficeService {
 
+    private final DoctorsOfficeRepository doctorsOfficeRepository;
     private final List<Office> offices = new ArrayList<>();
+
+    public OfficeService(DoctorsOfficeRepository doctorsOfficeRepository) {
+        this.doctorsOfficeRepository = doctorsOfficeRepository;
+    }
 
     public void deleteOffice(String officeId) {
         this.offices.removeIf(office -> office.getId().equals(officeId));
     }
 
     public Office createOffice(Office office) {
-        this.offices.add(office);
+        DoctorsOffice doctorsOffice = mapToDBEntity(office);
+        doctorsOfficeRepository.save(doctorsOffice);
         return office;
     }
 
-    public Optional<Office> findOffice(String officeId) {
-        return this.offices.stream().filter(office -> office.getId().equals(officeId)).findFirst();
+    public Office findOffice(String officeId) {
+        DoctorsOffice doctorsOffice = doctorsOfficeRepository.findByOfficeId(officeId);
+        return mapToDTO(doctorsOffice);
+    }
+
+    private DoctorsOffice mapToDBEntity(Office office) {
+        if (office == null) {
+            return null;
+        }
+
+        DoctorsOffice doctorsOffice = new DoctorsOffice();
+        doctorsOffice.setOfficeId(office.getId());
+        return doctorsOffice;
+    }
+
+    private Office mapToDTO(DoctorsOffice doctorsOffice) {
+        if (doctorsOffice == null) {
+            return null;
+        }
+
+        Office office = new Office();
+        office.setId(doctorsOffice.getOfficeId());
+        return office;
     }
 }
