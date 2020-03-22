c9a9da214ca04b97e6a11abe52bdee58b0ba3ef3
Philipp Endrulat <philipp.endrulat@thinkproject.com>
2020-03-22 08:59:48 +0100

    finished Queue. Now only scheduler needs to work

--- src/main/java/de/risikominimierungarztpraxen/queuingApp/service/NotificationSchedulerService.java
+++ src/main/java/de/risikominimierungarztpraxen/queuingApp/service/NotificationSchedulerService.java
@@ -1,5 +1,7 @@
 package de.risikominimierungarztpraxen.queuingApp.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 public class NotificationSchedulerService {
 
     private QueueService queueService;
+    private static final Logger LOG = LoggerFactory.getLogger(NotificationSchedulerService.class);
 
     @Autowired
     public NotificationSchedulerService(QueueService queueService) {
@@ -19,6 +22,7 @@ public class NotificationSchedulerService {
      */
     @Scheduled(cron = "0 * * ? * * *")
     public void removeOneMinuteFromAllAppointments() {
+        LOG.info("running scheduler");
         this.queueService.removeOneMinuteFromAllAppointments();
     }
 }
